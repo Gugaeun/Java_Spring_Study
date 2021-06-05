f5e9d5fc45b7a1046d52fb849cc91ef8880ba03e
구가은 <yms07050@naver.com>
2021-06-05 14:08:32 +0900

    0605 Spring

---
+++ SpringStudy210605/src/main/java/main/Main1.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import spring.Client;
+
+public class Main1 {
+    public static void main(String[] args) {
+        // 스프링(Spring) 빈(Bean) 라이프사이클과 범위
+        // 스프링 컨테이너(Container) 초기화와 종료
+        // 빈(Bean) 객체의 라이프사이클
+
+        AbstractApplicationContext ctx = new AnnotationConfigApplicationContext();
+        Client client = ctx.getBean(Client.class);
+        client.send();
+        ctx.close();
+    }
+}
