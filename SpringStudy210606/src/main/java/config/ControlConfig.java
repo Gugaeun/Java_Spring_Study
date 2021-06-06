c786937e9c49f416606057b31d66c3e0a7336df4
구가은 <yms07050@naver.com>
2021-06-06 18:20:02 +0900

    0606 Tomcat 을 사용한 Spring

--- SpringStudy210606/src/main/java/config/ControlConfig.java
+++ SpringStudy210606/src/main/java/config/ControlConfig.java
@@ -1,13 +1,18 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import mvcStudy.control.HelloSpringControl;
 import mvcStudy.control.MemberRegisterController;
+import springmvc.service.MemberRegisterService;
 
 @Configuration
 public class ControlConfig {
+	
+	@Autowired
+	private MemberRegisterService memberRegSvc;
 	
 	@Bean
 	public HelloSpringControl hellospringcontrol() {
@@ -16,6 +21,9 @@ public class ControlConfig {
 	
 	@Bean
 	public MemberRegisterController memberRegisterController() {
+		MemberRegisterController memberRegisterController = new MemberRegisterController();
+		memberRegisterController.setMemberRegisterService(memberRegSvc);
+		
 		return new MemberRegisterController();
 	}
 
